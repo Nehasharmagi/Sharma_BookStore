5c80cb2f437ff3758cf34794d475fb51d000c3b1
Neha Sharma <NS232@myscc.ca>
2022-03-11 05:23:43 -0500

    add Project

---
+++ SharmaBooks.Utility/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SharmaBooks.Utility.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Sharma_Books.Models/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sharma_Books.Models.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
